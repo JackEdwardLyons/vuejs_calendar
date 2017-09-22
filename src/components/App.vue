7ee11602fb6f560796d5c769300f7047c1a80049
Jack Lyons <jack.edward.lyons@gmail.com>
2017-09-22 21:05:49 +0200

    Add days to top of calendar grid

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,10 +1,31 @@
 <template>
-  <div id="calendar">
-    <div v-for="( week, i ) in weeks" class="calendar-week">
-      <CalendarDay v-for="day in week" :day="day"></CalendarDay>
+
+  <div>
+
+    <div id="day-bar">
+      <div>Mon</div>
+      <div>Tue</div>
+      <div>Wed</div>
+      <div>Thu</div>
+      <div>Fri</div>
+      <div>Sat</div>
+      <div>Sun</div>
     </div>
+
+    <div id="calendar">
+
+      <div v-for="( week, i ) in weeks" class="calendar-week">
+        
+        <CalendarDay v-for="day in week" :day="day"></CalendarDay>
+
+      </div>
+
+    </div>
+
   </div>
+
 </template>
+
 <script>
 import CalendarDay from './CalendarDay.vue'
 
