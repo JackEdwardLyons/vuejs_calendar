8c854afdd414f47ab0e8f03c7f7cd56b0b85784f
Jack Lyons <jack.edward.lyons@gmail.com>
2017-11-13 08:03:57 +1000

    Separate common JS logic from client and server. Aim is to create  server-entry and web-entry JS files for SSR

---
+++ src/entry.js
@@ -0,0 +1,66 @@
+/* 
+ * Shared file between web-entry.js & node-entry.js
+ * This file contains all the 'common logic'
+ */
+// Import Vue
+import Vue from 'vue';
+// Import Vuex Store
+import store from './store';
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+// Create an instance of moment on the Vue prototype
+// so all components can access the moment library.
+Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment; }});
+
+
+import App from './components/App.vue';
+// Return some mock data as part of the initial state
+let events = window.__INITIAL_STATE__.map(event => {
+  return {
+    description: event.description,
+    date: moment(event.date)
+  }
+});
+
+
+// Export Vue instance to run on client side.
+export default function( events ) {
+
+  // Merge an object of events into the store state
+  let initialState = Object.assign( {}, store.state, { events } );
+  console.log( initialState );
+  // Begin with a mock data state
+  store.replaceState( initialState );
+
+  return new Vue({
+    // The server renders the 'el: #app'
+    data: {
+      moment
+    },
+    components: {
+      App
+    },
+    store,
+    // Render function to create elements and meta data
+    // This means you can effectively skip the process of
+    // creating an HTML element in the index.html file.
+    render( createElement ) {
+      let vnode = createElement(
+        // 1st argument is the element
+        'div',
+        // 2nd argument are the attributes & meta
+        { attrs: { id: 'app' } },
+        // 3drd argument are the child nodes
+        [
+          createElement( 'app' )
+        ]
+      )
+      // We cannot remove the 'el: #app' from above
+      // as Vue needs to mount somewhere on the DOM
+      // that isn't the BODY tag. This render function
+      // just replaces the element on load.
+      return vnode;
+    }
+  });
+
+}
--- src/web.entry.js
+++ src/web.entry.js
@@ -1,16 +1,9 @@
-// Import Vue & Styles
-import Vue from 'vue';
+/* 
+ * Client Side Web Entry ( this code runs in the browser )
+ */
 import './style.scss';
-// Import Vuex Store
-import store from './store';
 import moment from 'moment-timezone';
 moment.tz.setDefault('UTC');
-// Create an instance of moment on the Vue prototype
-// so all components can access the moment library.
-Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment; }});
-
-import App from './components/App.vue';
-
 // Return some mock data as part of the initial state
 let events = window.__INITIAL_STATE__.map(event => {
   return {
@@ -18,39 +11,8 @@ let events = window.__INITIAL_STATE__.map(event => {
     date: moment(event.date)
   }
 });
-// Merge an object of events into the store state
-let initialState = Object.assign( {}, store.state, { events } );
-console.log( initialState );
-// Begin with a mock data state
-store.replaceState( initialState );
 
-new Vue({
-  el: '#app',
-  data: {
-    moment
-  },
-  components: {
-    App
-  },
-  store,
-  // Render function to create elements and meta data
-  // This means you can effectively skip the process of
-  // creating an HTML element in the index.html file.
-  render( createElement ) {
-    let vnode = createElement(
-      // 1st argument is the element
-      'div',
-      // 2nd argument are the attributes & meta
-      { attrs: { id: 'app' } },
-      // 3drd argument are the child nodes
-      [
-        createElement( 'app' )
-      ]
-    )
-    // We cannot remove the 'el: #app' from above
-    // as Vue needs to mount somewhere on the DOM
-    // that isn't the BODY tag. This render function
-    // just replaces the element on load.
-    return vnode;
-  }
-});
+import VueCalendar from './entry';
+// Because no 'el' was provided in the Vue instance
+// You must use the $mount instance method here.
+VueCalendar( events ).$mount( '#app' );
